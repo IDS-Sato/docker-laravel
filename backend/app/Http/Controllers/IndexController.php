7662a17a75700f5937be3033d619fdafc644190f
のぶりん <sato@idsnet.co.jp>
2021-06-18 19:40:58 +0900

    main

--- backend/app/Http/Controllers/IndexController.php
+++ backend/app/Http/Controllers/IndexController.php
@@ -21,13 +21,16 @@ class IndexController extends Controller
   public function index() {
     $constled = new constled;
     $constled = constled::first();
+    
+    /*
     $result = [
         'result'      => true,
         'const_no'     => $constled->const_no,
         'const_date' => $constled->const_date,
         'const_name' => $constled->const_name
     ];
-    
+    */
+
     $value = $constled->find(1);
     $arr = [$constled->const_no, $constled->const_date, $constled->const_name];
     return view('index', compact('value', 'arr'));
