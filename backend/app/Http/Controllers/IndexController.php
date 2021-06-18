92e6425504ef8724b9d731b8e918ce74dd1a192c
のぶりん <sato@idsnet.co.jp>
2021-06-18 18:57:24 +0900

    main

--- backend/app/Http/Controllers/IndexController.php
+++ backend/app/Http/Controllers/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\constled;
+
 class IndexController extends Controller
 {
     //
@@ -11,7 +13,16 @@ class IndexController extends Controller
 
     }
 
+    /*
     public function index(){
         return view('index');
     }
+    */
+  public function func() {
+    $constled = new constled;
+    $value = $constled->find(1);
+    $arr = ['const_no', 'const_date', 'const_name'];
+    return view('sample', compact('value', 'arr'));
+  }
+
 }
--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -1,1 +1,14 @@
-4-3:Hello World
+/*4-3:Hello World*/
+
+<html>
+  <head>
+    <title>HelloWorld</title>
+  </head>
+  <body>
+    <h1>HelloWorld!</h1>
+    <a>{{ $value['id'] }}</a>
+    <h1>{{ $arr[0] }}</h1>
+    <h2>{{ $arr[1] }}</h2>
+    <h3>{{ $arr[2] }}</h3>
+  </body>
+</html> 
