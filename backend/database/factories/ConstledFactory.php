d008f81c5f688f9a30d4644af863e241077cc83b
のぶりん <sato@idsnet.co.jp>
2021-06-18 16:21:11 +0900

    main

---
+++ backend/database/factories/ConstledFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\constled;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ConstledFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = constled::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
