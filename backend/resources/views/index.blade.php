beeac0f1fc2696dccccbd6ca9cddaaf2627e48b2
のぶりん <sato@idsnet.co.jp>
2021-06-18 19:35:34 +0900

    main

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -7,8 +7,8 @@
   <body>
     <h1>HelloWorld!</h1>
     <a>{{ $value['id'] }}</a>
-    <h1>{{ $arr[0] }}</h1>
-    <h2>{{ $arr[1] }}</h2>
-    <h3>{{ $arr[2] }}</h3>
+    <h1>{{ $const_no }}</h1>
+    <h2>{{ $const_date }}</h2>
+    <h3>{{ $const_name }}</h3>
   </body>
 </html> 
